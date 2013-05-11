82af7a22263c9eb487db453646235064a9b4c7d4
gauee <gauee@gauee-ThinkPad-Edge-E330.(none)>
2013-05-12 01:01:27 +0200

    Dodanie modelu Wiadomosci, utworzenie mozliwosci dodawania wydarzen do konta.

--- Pyganizer_Server/message_management/models.py
+++ Pyganizer_Server/message_management/models.py
@@ -1,8 +1,45 @@
 from django.db import models
+from user_management.models import UserPyGanizer
+import datetime
+#from dateutil import parser
 
 # Create your models here.
+
+class MsgManager(models.Manager):
+    def create_event(self,user,msgHeader,start,end,category):
+        baseMsg = Msg()
+        baseMsg.createTime = datetime.datetime.now()
+        baseMsg.msgHeader = msgHeader
+        baseMsg.userId = user
+        baseMsg.save()
+        extendMsg = Event()
+        extendMsg.msgId = baseMsg
+        extendMsg.start = start
+        extendMsg.end = end
+        extendMsg.category = category
+        extendMsg.save()
+        
+        return extendMsg
 
 class Msg(models.Model):
     createTime = models.DateTimeField('date published')
     msgHeader = models.CharField(max_length=100)
-    userId = models.ForeignKey('UserPyGanizer')
+    userId = models.ForeignKey(UserPyGanizer)
+    
+class StickyNote(models.Model):
+    msgId = models.OneToOneField('Msg')
+    endtime = models.DateTimeField('end_Time')
+    category = models.IntegerField()
+    objects = MsgManager()
+    
+class Event(models.Model):
+    msgId = models.OneToOneField('Msg')
+    start = models.DateTimeField('date_starting_event')
+    end = models.DateTimeField('date_ending_event')
+    category = models.IntegerField()
+    objects = MsgManager()
+    
+class Comment(models.Model):
+    msgId = models.OneToOneField('Msg')
+    mainText = models.CharField(max_length=1000)
+    #TODO drzewko komentarzy
--- Pyganizer_Server/message_management/views.py
+++ Pyganizer_Server/message_management/views.py
@@ -1,6 +1,35 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.template import RequestContext
+from django.shortcuts import render, render_to_response, redirect
+from message_management.models import Event
 
 def add_message(request):
+    return render_to_response('add_message.html', RequestContext(request))
+
+def add_event(request):
+    header = request.POST.get('msg_header')
+    desc = request.POST.get('msg_desc')
+    start = request.POST.get('msg_start')
+    end = request.POST.get('msg_end')
+    
+    user = request.user.profile
+    
+    Event.objects.create_event(user,header,start,end,1)
+    
     return redirect("/")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   

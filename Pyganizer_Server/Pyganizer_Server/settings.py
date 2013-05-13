1042492108a1cba8280951fd088b49476f44cbd0
gauee <gauee@gauee-ThinkPad-Edge-E330.(none)>
2013-05-13 23:16:52 +0200

    Dodanie plików statycznych oraz skryptu czyszczącego repo lokalne z plikow .pyc

--- Pyganizer_Server/Pyganizer_Server/settings.py
+++ Pyganizer_Server/Pyganizer_Server/settings.py
@@ -5,6 +5,8 @@ from django.conf.global_settings import FIXTURE_DIRS
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+PWD=os.path.dirname(os.path.realpath(__file__))
+
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -58,13 +60,13 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = PWD
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -75,6 +77,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PWD,"static"),
 )
 
 # List of finder classes that know how to find static files in
@@ -110,7 +113,6 @@ ROOT_URLCONF = 'Pyganizer_Server.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'Pyganizer_Server.wsgi.application'
 
-PWD=os.path.dirname(os.path.realpath(__file__))
 
 TEMPLATE_DIRS = (
     os.path.join(PWD,"templates"),
